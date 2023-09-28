7ca483991301ccd67776f28a769a71bc909a27bf
crmsharks <138724406+crmsharks@users.noreply.github.com>
2023-09-28 10:47:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+document.addEventListener('DOMContentLoaded', () => {
+    const shark = document.getElementById('shark');
+    const integration = document.getElementById('integration');
+    const startButton = document.getElementById('start-button');
 
-\f0\fs24 \cf0 document.addEventListener('DOMContentLoaded', () => \{\
-    const shark = document.getElementById('shark');\
-    const ground = document.getElementById('ground');\
-    const startButton = document.getElementById('start-button');\
-\
-    let isJumping = false;\
-\
-    startButton.addEventListener('click', startGame);\
-    document.addEventListener('keydown', jump);\
-\
-    function startGame() \{\
-        startButton.style.display = 'none';\
-        document.addEventListener('keydown', jump);\
-        shark.style.animation = 'swim 0.5s infinite alternate';\
-        ground.style.animation = 'scroll 2s linear infinite';\
-    \}\
-\
-    function jump(event) \{\
-        if (event.keyCode === 32 && !isJumping) \{\
-            isJumping = true;\
-            shark.style.animation = 'none';\
-            shark.style.transform = 'translateY(-30px)';\
-            setTimeout(() => \{\
-                shark.style.transform = 'translateY(0)';\
-                shark.style.animation = 'swim 0.5s infinite alternate';\
-                isJumping = false;\
-            \}, 500);\
-        \}\
-    \}\
-\});\
-}
+    let isPlaying = false;
+    let score = 0;
+
+    startButton.addEventListener('click', startGame);
+    document.addEventListener('keydown', jump);
+
+    function startGame() {
+        startButton.style.display = 'none';
+        document.addEventListener('keydown', jump);
+        isPlaying = true;
+        animateIntegration();
+    }
+
+    function jump(event) {
+        if (event.keyCode === 32 && !isPlaying) {
+            isPlaying = true;
+            shark.style.animation = 'none';
+            shark.style.transform = 'translateY(-50px)';
+            setTimeout(() => {
+                shark.style.transform = 'translateY(0)';
+                shark.style.animation = 'swim 0.5s infinite alternate';
+                isPlaying = false;
+            }, 500);
+        }
+    }
+
+    function animateIntegration() {
+        if (isPlaying) {
+            const randomPosition = Math.random() * 280;
+            integration.style.left = randomPosition + 'px';
+            integration.style.animation = 'move 2s linear forwards';
+            integration.addEventListener('animationend', () => {
+                integration.style.animation = 'none';
+                score++;
+                if (score % 5 === 0) {
+                    increaseDifficulty();
+                }
+                document.getElementById('score').textContent = 'Score: ' + score;
+                animateIntegration();
+            });
+        }
+    }
+
+    function increaseDifficulty() {
+        // You can make the game more challenging by adjusting animation speed, adding more obstacles, etc.
+    }
+});
